d2a600e769cbb4fcdc857039e01e9590a24b0a6a
HoLaw77 <117204732+HoLaw77@users.noreply.github.com>
2023-05-27 16:28:30 +0000

    amend function

--- reduce.js
+++ reduce.js
@@ -7,7 +7,7 @@
 
 // Summing an array of numbers:
 const num = [0, 1, 2, 3, 4];
-let sum = num.reduce((acc, curr)=> (acc + curr, 0))
+let sum = num.reduce((acc, curr)=> acc + curr, 0)
 
 console.log(sum)
 
